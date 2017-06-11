8190d630d835b4672aeeac21cd4926772ba1f32c
Patrick Schmolke <grauertod@googlemail.com>
2017-06-11 22:25:14 +0200

    remove unused imports

--- new-worlds/src/main/java/de/awesome/corporate/newworlds/core/planets/entity/Planet.java
+++ new-worlds/src/main/java/de/awesome/corporate/newworlds/core/planets/entity/Planet.java
@@ -1,9 +1,6 @@
 package de.awesome.corporate.newworlds.core.planets.entity;
 
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
 @Entity
 public class Planet extends AbstractPlanetoid {
