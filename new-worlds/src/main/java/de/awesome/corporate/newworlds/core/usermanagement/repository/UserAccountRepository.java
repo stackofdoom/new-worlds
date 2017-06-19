f3fb805b3d227983c0be32c864594f91ba4237c2
Patrick Schmolke <grauertod@googlemail.com>
2017-06-20 01:19:52 +0200

    nothing

--- new-worlds/src/main/java/de/awesome/corporate/newworlds/core/usermanagement/repository/UserAccountRepository.java
+++ new-worlds/src/main/java/de/awesome/corporate/newworlds/core/usermanagement/repository/UserAccountRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import de.awesome.corporate.newworlds.core.usermanagement.entity.UserAccount;
 
-public interface UserAccountRepository extends JpaRepository<UserAccount,String> {
+public interface UserAccountRepository
+        extends JpaRepository<UserAccount, String> {
 
 }
