c1723a009dea332272f66f4a9e95cc725776e94d
Patrick Schmolke <grauertod@googlemail.com>
2017-09-05 09:04:22 +0200

    basic structure for citites

--- new-worlds/src/main/java/de/awesome/corporate/newworlds/core/planets/city/City.java
+++ new-worlds/src/main/java/de/awesome/corporate/newworlds/core/planets/city/City.java
@@ -1,10 +1,16 @@
 package de.awesome.corporate.newworlds.core.planets.city;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 
 import de.awesome.corporate.newworlds.core.types.AbstractNamedEntity;
 
 @Entity
 public class City extends AbstractNamedEntity{
+	
+	
+	
+	private List<Structure> structures;
 
 }
---
+++ new-worlds/src/main/java/de/awesome/corporate/newworlds/core/planets/city/CityProperties.java
@@ -0,0 +1,7 @@
+package de.awesome.corporate.newworlds.core.planets.city;
+
+public class CityProperties {
+	
+	private long population;
+
+}
